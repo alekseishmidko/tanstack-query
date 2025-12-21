73f9b101145953db83408526ef89c3ab24d57c9e
alekseishmidko <alexeishmidko@gmail.com>
2025-12-21 22:08:26 +0700

    stale time, garbage collection settings, enable fetch

--- src/shared/api/query-client.ts
+++ src/shared/api/query-client.ts
@@ -1,3 +1,14 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      //время, пока данные считаются свежими, в этот период не будет автоматического refetch
+      staleTime: 60 * 1000,
+      //время, после которого кешированные данные удаляются,отсчитывается с момента, когда последний наблюдатель (компонент) отписался
+      gcTime: 5 * 60 * 1000,
+      retry: 2,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
